379317c72d45846b3d0542eaade4a024e75a5d81
kamran ul haq <kamranulhaq2002@gmail.com>
2024-11-21 03:38:34 +0500

    Time: 1 ms (71.36%), Space: 21.8 MB (76.22%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,6 +5,9 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        
+        # end = len(nums) - k # 7-3 = 4
+        # # nums = nums[end:] + nums[:end]
         n = len(nums)
         k = k % n
         nums[:] = nums[n-k:] + nums[:n-k]
