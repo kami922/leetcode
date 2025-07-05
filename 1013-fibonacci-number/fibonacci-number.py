1e986754219dec7e56554a61340e87bf9e3bafd2
kamran ul haq <kamranulhaq2002@gmail.com>
2025-07-05 22:35:20 +0500

    Time: 323 ms (30.95%) | Memory: 17.8 MB (23.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        #base case
         if n <= 1:
             return n
-        last = self.fib(n-1)
-        slast = self.fib(n-2)
-        return last+slast
+        return self.fib(n-1) + self.fib(n-2)
